43323828ebd6427f2cfa2fcaa569e850f6126788
Evan Kooyer <echofish626@protonmail.com>
2024-08-31 12:00:01 -0500

    decided to make the trivia questions function a generator to save on resources while the app is running.

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import requests
 import json
+import random
+
+session_token = None
 
 
+# Generator function to pull and parse trivia questions for program use
 def get_trivia_questions(num_qs=10, category='general'):
     api_response = requests.get(
-        'https://opentdb.com/api.php?amount={q}'.format(q=num_qs))
+        'https://opentdb.com/api.php?amount={q}'.format(q=num_qs)
+    )
+
     trivia_data = json.loads(api_response.text)
 
-    return trivia_data['results']
+    for question in trivia_data['results']:
+        options = list(question['incorrect_answers'])
+        options.append(question['correct_answer'])
+        random.shuffle(options)
+
+        question_dict = {
+            'category': question['category'],
+            'difficulty': question['difficulty'],
+            'question': question['question'],
+            'options': options,
+            'correct_answer': question['correct_answer']
+        }
+        yield question_dict
 
 
 trivia_questions = get_trivia_questions()
 
 for question in trivia_questions:
-    print(question['category'])
-    print(question['difficulty'])
-    print(question['question'])
+    print(question)
     print('\n')
