4b5fd7febbbbd6e0826774bfc2a0955585bd487c
Evan Kooyer <echofish626@protonmail.com>
2024-08-30 22:27:12 -0500

    defined basic function to retrieve 10 random questions from openTDB
    corrected email address for commit
    
    fixed configuration

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from textual.app import App
-
-class TriviaPy(App):
-    pass
+import requests
+import json
 
 
+def get_trivia_questions(num_qs=10, category='general'):
+    api_response = requests.get(
+        'https://opentdb.com/api.php?amount={q}'.format(q=num_qs))
+    trivia_data = json.loads(api_response.text)
+
+    return trivia_data['results']
+
+
+trivia_questions = get_trivia_questions()
+
+for question in trivia_questions:
+    print(question['category'])
+    print(question['difficulty'])
+    print(question['question'])
+    print('\n')
