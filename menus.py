4834f5d0f346cf3b7f91f95277582e798c20b0cf
Evan Kooyer <echofish626@protonmail.com>
2024-09-12 22:06:35 -0500

    added a scoring system to the game and started working out a screen for summarizing the game

--- menus.py
+++ menus.py
@@ -1,3 +1,4 @@
+from textual import on
 from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.widgets import Label, Button, Static, Footer, Header
@@ -9,6 +10,22 @@ import main
 import time
 
 
+class GameFinish(Screen):
+
+    def __init__(self, score, correct, incorrect, id):
+        self.final_score = score
+        self.correct_answers = correct
+        self.incorrect_answers = incorrect
+        super.__init__(id=id)
+
+    def __compose__(self) -> ComposeResult:
+        yield Label('RESULTS', id='results')
+        yield Label('Correct Answers: {correct}'.format(correct=self.correct_answers), id='correct')
+        yield Label('Incorrect Answers: {incorrect}'.format(incorrect=self.incorrect_answers), id='incorrect')
+        yield Label('Final Score: {score}'.format(score=self.final_score))
+        yield Button('Return to Main Menu', id='return')
+
+
 class Question(Static):
     # Question class takes in a question and the current score and shows it on screen
 
@@ -17,25 +34,36 @@ class Question(Static):
 
     def __init__(self, question, id: str, current_score=0):
         self.question = question
+        self.score = current_score
+        self.started = time.time()
         super().__init__()
 
     class Answered(Message):
 
-        def __init__(self, correct: bool) -> None:
+        def __init__(self, correct: bool, points: int, question) -> None:
             self.correct = correct
+            self.points = points
+            self.question = question
             super().__init__()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if self.option_dict[event.button.id] == self.question['correct_answer']:
             self.correct = True
-            self.post_message(self.Answered(self.correct))
+            self.stopped = time.time()
+            modifier = round(((30-(self.stopped-self.started))/30), ndigits=4)
+            score = int(1000*modifier)
+            self.post_message(self.Answered(
+                self.correct, score, self.question))
         else:
             self.correct = False
-            self.post_message(self.Answered(self.correct))
+            self.post_message(self.Answered(self.correct, 0, self.question))
 
+    # Don't understand why this decorator works but its the only way my program
+    # recognized this event...
+    @on(Countdown.Finished)
     def on_countdown_finish(self, event: Countdown.Finished) -> None:
         self.correct = False
-        self.post_message(self.Answered(self.correct))
+        self.post_message(self.Answered(self.correct, 0, self.question))
 
     def compose(self) -> ComposeResult:
         yield Label(self.question['question'], id='question')
@@ -47,12 +75,12 @@ class Question(Static):
             self.option_dict['a_' + str(button_num)] = option
             yield Button(option, id=('a_' + str(button_num)))
             button_num += 1
-
+        yield Label('Current Score: {score}'.format(score=self.score))
         # this countdown timer is not quite working well for me, this
         # is the only way i have gotten it to work so far.
-        cd = Countdown(id='countdown')
-        yield cd
-        cd.start(30)
+        self.cd = Countdown(id='countdown')
+        yield self.cd
+        self.cd.start(30)
 
 
 class Game(Screen):
@@ -61,20 +89,37 @@ class Game(Screen):
         self.gtqs = gtqs
         self.question_ans = 0
         self.score = 0
+        self.correct = 0
+        self.incorrect = 0
         self.num_qs = num_qs
         super().__init__(id=id)
+
+    class Finished(Message):
+
+        def __init__(self, score, correct, incorrect):
+            self.final_score = score
+            self.num_correct = correct
+            self.num_incorrect = incorrect
+            super.__init__()
 
     def on_question_answered(self, message: Question.Answered) -> None:
         if message.correct is True:
             self.question_ans += 1
-            self.score += 1
+            self.correct += 1
+            self.score += message.points
         else:
+            self.incorrect += 1
             self.question_ans += 1
 
         if self.question_ans < self.num_qs:
             self.remove_children()
             self.mount(Question(next(self.gtqs),
-                       id='q_{num}'.format(num=self.question_ans)))
+                       id='q_{num}'.format(num=self.question_ans), current_score=self.score))
+        else:
+            self.remove_children()
+            self.install_screen(GameFinish(
+                self.score, self.correct, self.incorrect, id='finished'))
+            self.push_screen('finished')
 
     def compose(self) -> ComposeResult:
 
